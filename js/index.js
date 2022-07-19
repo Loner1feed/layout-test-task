ebe23c8268d33f7e2d4178e7888279d87117e8f0
Eugen Guriev <gto200313@gmail.com>
2022-07-20 01:18:55 +0300

    layout work done

--- js/index.js
+++ js/index.js
@@ -15,4 +15,43 @@ $(document).ready(function() {
     $(this).toggleClass('dropInfo__heading--open')
   })
 
+  // slider 
+  $('.steps__slider').slick({
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+    centerMode: true,
+    infinite: false,
+    appendArrows: '.steps__arrows',
+    prevArrow: '<a href="javascript://" class="arrow arrow--prev"><span class="icon-chevron"></span></a>',
+    nextArrow: '<a href="javascript://" class="arrow arrow--next"><span class="icon-chevron"></span></a>',
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+          // infinite: true,
+        }
+      }
+    ]
+  })
+
+
+  // mobile slider 
+  if($(window).width() <= '600') {
+    $('.portfolio__grid').slick({
+      rows: 3,
+      prevArrow: '<a href="javascript://" class="arrow arrow--prev"><span class="icon-chevron"></span></a>',
+      nextArrow: '<a href="javascript://" class="arrow arrow--next"><span class="icon-chevron"></span></a>',
+      appendArrows: '.portfolio__arrows',
+    })
+  }
+
 })
